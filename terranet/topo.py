47d84337deeac9808d96018835ff1ed57248e648
Martin Thomas Fleischer <martin.fleischer@campus.tu-berlin.de>
2019-07-24 01:08:10 +0200

    [refactor] Snakecase add methods, rename templates
    Snakecase method names are more pythonic. Fixes the new class names in
    the add methods. Rename also the templates and use the full name instead
    of cryptic short names.

--- terranet/topo.py
+++ terranet/topo.py
@@ -20,45 +20,48 @@ class Terratopo(IPTopo):
         router = self.addNode(name, isRouter=True, **kwargs)
         return TerranetRouterDescription(router, self)
 
-    def addCN(self, name, **opts):
-        return self.addRouter(name, isCN=True, cls=CN, **opts)
+    def add_client_node(self, name, **opts):
+        return self.addRouter(name, is_client_node=True, cls=ClientNode,
+                              **opts)
 
-    def addDN60(self, name, **opts):
-        return self.addRouter(name, isDN60=True, cls=DN60, **opts)
+    def add_distribution_node_60(self, name, **opts):
+        return self.addRouter(name, is_distribution_node60=True,
+                              cls=DistributionNode60, **opts)
 
-    def addDN5_60(self, name, **opts):
-        return self.addRouter(name, isDN5_60=True, cls=DN5_60, **opts)
+    def add_distribution_node_5_60(self, name, **opts):
+        return self.addRouter(name, is_distribution_node_5_60=True,
+                              cls=DistributionNode5_60, **opts)
 
-    def addGateway(self, name, **opts):
+    def add_gateway(self, name, **opts):
         return self.addSwitch(name, cls=Gateway, **opts)
 
-    def addIperfDownloadClient(self, name, server_name=None, **opts):
+    def add_iperf_download_client(self, name, server_name=None, **opts):
         return self.addHost(name, cls=IperfDownloadClient,
                 server_name=server_name, **opts)
 
-    def addIperfDownloadServer(self, name, **opts):
+    def add_iperf_download_server(self, name, **opts):
         return self.addHost(name, cls=IperfDownloadServer, **opts)
 
-    def isCN(self, node):
-        return self.isNodeType(node, 'isCN')
+    def is_client_node(self, node):
+        return self.isNodeType(node, 'is_client_node')
 
-    def isDN5_60(self, node):
-        return self.isNodeType(node, 'isDN5_60')
+    def is_distribution_node_5_60(self, node):
+        return self.isNodeType(node, 'is_distribution_node_5_60')
 
-    def isDN5_60(self, node):
-        return self.isNodeType(node, 'isDN60')
+    def is_distribution_node_60(self, node):
+        return self.isNodeType(node, 'is_distribution_node_60')
 
     def cns(self, sort=True):
-        return filter(self.isCN, self.nodes(sort))
+        return filter(self.is_client_node, self.nodes(sort))
 
     def dns(self, sort=True):
         return self.dn5_60s(sort=sort) + self.dn60s(sort=sort)
 
     def dn5_60s(self, sort=True):
-        return filter(self.isDN5_60, self.nodes(sort))
+        return filter(self.is_distribution_node_5_60, self.nodes(sort))
 
     def dn60s(self, sort=True):
-        return filter(self.isDN60, self.nodes(sort))
+        return filter(self.is_distribution_node_60, self.nodes(sort))
 
     def terranodes(self, sort=True):
         return cns(sort=sort) + dns(sort=sort)
