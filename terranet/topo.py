a838e6916d3a1c5da70095091f103a2586671fa7
Martin Thomas Fleischer <martin.fleischer@campus.tu-berlin.de>
2020-09-17 18:50:42 +0200

    [Terratopo] Simplify add_customer_flow
    Simplify the interface of the add_customer_flow method. Add some default
    values for the ip address assignment.

--- terranet/topo.py
+++ terranet/topo.py
@@ -1,4 +1,7 @@
+import uuid
 import itertools
+from ipaddress import IPv4Network, IPv6Network
+
 from ipmininet.link import IPLink
 from ipmininet.iptopo import IPTopo
 from .router_config import OpenrConfig, TerranetRouterDescription
@@ -86,3 +89,62 @@ class Terratopo(IPTopo):
 
     def terranodes(self, sort=True):
         return client_nodes(sort=sort) + distribution_nodes(sort=sort)
+
+    def _get_customer_ip(self, ip):
+        cn_v4_net = IPv4Network(ip[0].decode('utf-8'), strict=False)
+        customer_v4_net = next(
+            itertools.islice(cn_v4_net.subnets(), 1, None))
+        customer_v4_ip = "{netaddr}/{prefix}".format(
+                netaddr=str(customer_v4_net.network_address),
+                prefix=cn_v4_net.prefixlen)
+
+        cn_v6_net = IPv6Network(ip[1].decode('utf-8'), strict=False)
+        customer_v6_net = next(
+            itertools.islice(cn_v6_net.subnets(), 1, None))
+        customer_v6_ip = "{netaddr}/{prefix}".format(
+                netaddr=str(customer_v6_net.network_address),
+                prefix=cn_v6_net.prefixlen)
+        return (customer_v4_ip, customer_v6_ip)
+
+    def _get_ip(self, network,
+                 cn_prefix4=24,
+                 cpe_prefix4=16,
+                 cn_prefix6=80,
+                 cpe_prefix6=64):
+        net4 = IPv4Network(network[0])
+        cn4 =  "{}/{}".format(net4[1], cn_prefix4)
+        cpe4 = "{}/{}".format(net4[2], cpe_prefix4)
+
+        net6 = IPv6Network(network[1])
+        cn6 = "{}/{}".format(net6[1], cn_prefix6)
+        cpe6 = "{}/{}".format(net6[2], cpe_prefix6)
+        return ((cn4, cn6), (cpe4, cpe6))
+
+    def add_customer_flow(self,
+                          cn,
+                          gateway,
+                          suffix=None,
+                          client_prefix="h",
+                          server_prefix="s",
+                          network=("10.0.0.0/24",
+                                   "fd00:0:0:8000:8000::0/80"),
+                          autostart_client=False,
+                          autostart_server=True,
+                          *args, **kwargs):
+        if not suffix:
+            suffix_len = 10 - max(len(client_prefix), len(server_prefix))
+            suffix = uuid.uuid4().hex[:suffix_len]
+        cpe_name = "{}{}".format(client_prefix, suffix)
+        server_name = "{}{}".format(server_prefix, suffix)
+        cpe = self.add_iperf_reverse_client(cpe_name,
+                                            server_name,
+                                            autostart=autostart_client)
+        link = self.add_wifi_link(cn, cpe)
+        (cn_ip, cpe_ip) = self._get_ip(network)
+        link[cn].addParams(ip=cn_ip)
+        link[cpe].addParams(ip=cpe_ip)
+
+        server = self.add_iperf_server(server_name,
+                                       autostart=True)
+        self.add_ip_link(gateway, server)
+        return (cpe, server)
