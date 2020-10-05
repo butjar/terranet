f8286b0ee51daab7af81d70423908bb9aed2f995
Martin Thomas Fleischer <martin.fleischer@campus.tu-berlin.de>
2020-10-05 12:15:20 +0200

    [ryu] Isolate CustomerMonitor
    The customer monitor can be implemented separately from mac learning and
    table matching. Query the table stats to find active tables to request
    flow stats from.

--- terranet/ryu/ryu/app/customer_monitor.py
+++ terranet/ryu/ryu/app/customer_monitor.py
@@ -1,31 +1,33 @@
+from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER, DEAD_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.lib import hub
+from ryu.controller import dpset
 
 from influxdb import InfluxDBClient
 
-from .customer_flow_matching import CustomerFlowMatching
 
-
-class CustomerMonitor(CustomerFlowMatching):
-    """
+class CustomerMonitor(app_manager.RyuApp):
+    '''
     https://osrg.github.io/ryu-book/en/html/traffic_monitor.html
-    """
+    '''
 
-    INTERVAL = 10
-    DB_NAME = "switchstats"
+    INTERVAL = 5
+    DB_NAME = 'switchstats'
 
     def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
         self.datapaths = {}
-        self.monitor_thread = hub.spawn(self._monitor)
+        self.table_ids = []
         self._init_influxdb()
-        super(CustomerMonitor, self).__init__(*args, **kwargs)
+        self.monitor_thread = hub.spawn(self._monitor)
 
     def _init_influxdb(self):
         dbname = self.__class__.DB_NAME
-        self.influxclient = InfluxDBClient(username="admin",
-                                           password="admin",
+        self.influxclient = InfluxDBClient(username='admin',
+                                           password='admin',
                                            database=dbname)
         self.influxclient.drop_database(dbname)
         self.influxclient.create_database(dbname)
@@ -108,77 +110,102 @@ class CustomerMonitor(CustomerFlowMatching):
         datapath = ev.datapath
         if ev.state == MAIN_DISPATCHER:
             if datapath.id not in self.datapaths:
-                self.logger.debug("CustomerMonitor: Register "
-                                  "datapath {}.".format(datapath.id))
+                self.logger.info('CustomerMonitor: Register '
+                                 'datapath {}.'.format(datapath.id))
                 self.datapaths[datapath.id] = datapath
         elif ev.state == DEAD_DISPATCHER:
             if datapath.id in self.datapaths:
-                self.logger.debug("CustomerMonitor: Unregister "
-                                  "datapath {}.".format(datapath.id))
+                self.logger.info('CustomerMonitor: Unregister '
+                                 'datapath {}.'.format(datapath.id))
                 del self.datapaths[datapath.id]
 
     def _monitor(self):
         while True:
             for dp in self.datapaths.values():
+                self._request_table_stats(dp)
                 self._request_customer_stats(dp)
             hub.sleep(self.__class__.INTERVAL)
 
+    def _request_table_stats(self, datapath):
+        self.logger.debug('CustomerMonitor: Sending TableStatsRequest to '
+                          'datapath {}.'.format(datapath.id))
+        ofp_parser = datapath.ofproto_parser
+
+        req = ofp_parser.OFPTableStatsRequest(datapath, 0)
+        datapath.send_msg(req)
+
+    @set_ev_cls(ofp_event.EventOFPTableStatsReply, MAIN_DISPATCHER)
+    def _table_stats_reply_handler(self, ev):
+        self.logger.debug('CustomerMonitor: TableStatsReply received.')
+        for stat in ev.msg.body:
+            table_id = stat.table_id
+            if (not self._is_customer_table(table_id) or
+                not stat.active_count):
+                continue
+            if not table_id in self.table_ids:
+                self.table_ids.append(table_id)
+                self.logger.info('CustomerMonitor: Added id {} to table_ids.'
+                                  .format(table_id))
+
+    def _is_customer_table(self, table_id):
+        return (table_id > 3 and
+                table_id < 100)
+
     def _request_customer_stats(self, datapath):
-        self.logger.debug("CustomerMonitor: Sending stats request to "
-                          "datapath {}.".format(datapath.id))
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        self.logger.debug("{}".format(self.customer_table_allocation))
-        for dn_id, values in self.customer_table_allocation.items():
-            table_id = values["table_id"]
+        for table_id in self.table_ids:
             req = parser.OFPFlowStatsRequest(datapath, table_id=table_id)
             datapath.send_msg(req)
-            self.logger.info("CustomerMonitor: "
-                             "Requested table stats for DN {} "
-                             "at table_id {}.".format(dn_id,
-                                                      table_id))
+            self.logger.debug('CustomerMonitor: '
+                              'FlowStatsRequest send for table_id {} '
+                              'to datapath {}.'
+                              .format(table_id, datapath.id))
 
     @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
     def _flow_stats_reply_handler(self, ev):
+        self.logger.debug('CustomerMonitor: FlowStatsReply received.')
         body = ev.msg.body
 
         for stat in [flow for flow in body if flow.priority > 1]:
-            if stat.match.get("ipv6_src"):
-                ipv6 = stat.match["ipv6_src"]
-                measurements = ("packet_count_out", "byte_count_out")
-
-            if stat.match.get("ipv6_dst"):
-                ipv6 = stat.match["ipv6_dst"]
-                measurements = ("packet_count_in", "byte_count_in")
-
-            datapoints = [
-                {
-                    "measurement": measurements[0],
-                    "tags": {
-                        "ipv6": ipv6
-                    },
-                    "fields": {
-                        "value": stat.packet_count
-                    }
-                },
-                {
-                    "measurement": measurements[1],
-                    "tags": {
-                        "ipv6": ipv6
-                    },
-                    "fields": {
-                        "value": stat.byte_count
-                    }
-                }
-            ]
-
-            self.influxclient.write_points(datapoints)
-            self.logger.info("CustomerMonitor: Received stats: "
-                             "table_id {}, "
-                             "match {}, "
-                             "packet_count {}, "
-                             "byte_count {}.".format(stat.table_id,
+            self.logger.info('CustomerMonitor: Received stats: '
+                             'table_id {}, '
+                             'match {}, '
+                             'packet_count {}, '
+                             'byte_count {}.'.format(stat.table_id,
                                                      stat.match,
                                                      stat.packet_count,
                                                      stat.byte_count))
+
+            if stat.match.get('ipv6_src'):
+                ipv6 = stat.match['ipv6_src']
+                measurements = ('packet_count_out', 'byte_count_out')
+
+            if stat.match.get('ipv6_dst'):
+                ipv6 = stat.match['ipv6_dst']
+                measurements = ('packet_count_in', 'byte_count_in')
+
+            if measurements:
+                datapoints = [
+                    {
+                        'measurement': measurements[0],
+                        'tags': {
+                            'ipv6': ipv6
+                        },
+                        'fields': {
+                            'value': stat.packet_count
+                        }
+                    },
+                    {
+                        'measurement': measurements[1],
+                        'tags': {
+                            'ipv6': ipv6
+                        },
+                        'fields': {
+                            'value': stat.byte_count
+                        }
+                    }
+                ]
+
+                self.influxclient.write_points(datapoints)
