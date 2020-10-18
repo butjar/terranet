f01a043fdbadde4237107ef6bb0b0b0286c7f223
Martin Thomas Fleischer <martin.fleischer@campus.tu-berlin.de>
2020-10-18 15:15:33 +0200

    [examples] Refactor hybrid handover scenario
    Add a third DN to show the station fairness problem.

--- terranet/simulation/simulate_hybrid_handover.py
+++ terranet/simulation/simulate_hybrid_handover.py
@@ -14,37 +14,39 @@ if __name__ == '__main__':
                    build=False)
     module_path = 'terranet.ryu.app'
     apps = ('customer_flow_pipeline',
-            'customer_stats_monitor')
+            'customer_stats_monitor',
+            'customer_allocation_monitor')
     modules = [ '.'.join([module_path, x]) for x in apps ]
     ryu_manager = RyuManager('ryu-manager', modules=modules)
     net.addController(ryu_manager)
     with TerranetSimulator('handover_simulation', net, clean_after=False) as net:
-        # DN_A <-> CN_A1
+        # DN_A <=> CN_A1
         dn_a = net['dn_a']
         cn_a1 = net['cn_a1']
         link_dn_a_cn_a1 = net.linksBetween(dn_a, cn_a1)[0]
-        # DN_B <-> DN_B1
+        # DN_B <=> DN_B1
         dn_b = net['dn_b']
         cn_b1 = net['cn_b1']
-        cn_b2 = net['cn_b2']
         link_dn_b_cn_b1 = net.linksBetween(dn_b, cn_b1)[0]
-        link_dn_b_cn_b2 = net.linksBetween(dn_b, cn_b2)[0]
-        time.sleep(30)
-
-        info('{}: Simulating link failure DN_A <=>
-                CN_A1\n'
-                .format(TerranetSimulator.time_ns()))
-        link_dn_a_cn_a1.intf2.ifconfig('down')
+        # DN_C <=> DN_C1
+        dn_c = net['dn_c']
+        cn_c1 = net['cn_c1']
+        link_dn_c_cn_c1 = net.linksBetween(dn_c, cn_c1)[0]
         time.sleep(60)
+
+        info('{}: Simulating link failure DN_A <=> CN_A1\n'
+             .format(TerranetSimulator.time_ns()))
+        link_dn_a_cn_a1.intf2.ifconfig('down')
+        time.sleep(120)
 
         info('{}: Simulating link failure DN_B <=> CN_B1 '
-             'and DN_B <=> CN_B2\n'
+             'and DN_C <=> CN_C1\n'
              .format(TerranetSimulator.time_ns()))
         link_dn_b_cn_b1.intf2.ifconfig('down')
-        link_dn_b_cn_b2.intf2.ifconfig('down')
-        time.sleep(60)
+        link_dn_c_cn_c1.intf2.ifconfig('down')
+        time.sleep(120)
 
         info('{}: Simulating link failure recovery DN_A <=> CN_A1\n'
              .format(TerranetSimulator.time_ns()))
         link_dn_a_cn_a1.intf2.ifconfig('up')
-        time.sleep(30)
+        time.sleep(60)
--- terranet/topo/hybrid_backup_topo.py
+++ terranet/topo/hybrid_backup_topo.py
@@ -6,55 +6,55 @@ class HybridBackupTerragraphTopo(Terratopo):
     mmWave Distribution Network backhaul
     ====================================
 
-            +-----+            +-----+
-            |cn_a1|            |cn_b1|
-            +-----+            +-----+
-      5GHz .   |                  |   . 5GHz
-          .                            .
-    +----+     |                  |      +----+
-    |ap_a|      60GHz        60GHz       |ap_b|
-    +----+     |                  |      +----+
-          \                            /
-     cable \   |                  |   / cable
-            +------+   60GHz  +------+
-            | dn_a |- - - - - | dn_b |
-            +------+          +------+
+            +-----+            +-----+           +-----+
+            |cn_a1|            |cn_b1|           |cn_c1|
+            +-----+            +-----+           +-----+
+      5GHz .   |             .    |            .    |
+          .                 .                 .
+    +----+     |      +----+      |     +----+      |
+    |ap_a|   60GHz    |ap_b|    60GHz   |ap_c|    60GHz
+    +----+     |      +----+      |     +----+      |
+          \                 \                 \ 
+     cable \   |             \    |            \    |
+            +------+  60GHz   +------+  60GHz   +------+
+            | dn_a |- - - - - | dn_b |- - - - - | dn_c |
+            +------+          +------+          +------+
                 |
                 | cable
                 |
             + -_-_- +
             |  _X_  | gw
             + - - - +
-             | |  | |
-             | |  |  \  cable
-             .-~~~-~. \ 
-     .- ~ ~-(| |  |    \)_ _
-    /        / |   \    \   ~ -.
-   |     s1a1 s1a2 s1b1 s2b1    \ 
+              | | |
+              | | | cable
+             .-~~~-~.
+     .- ~ ~-(         )_ _
+    /                      ~ -.
+   |  s1a1 s1b1 s1c1 s2c1 s3c1  \ 
     \                        _.~'
       ~- . ______________ . -
 
 
 
     WiFi backup edge
-    =========
+    ================
 
-        h1a1 h2a1 h3a1    h1b1 h2b1 h3b1    (CPEs)
-          \    |    /       \    |    /
-       ^   \   |   /         \   |   /      -- cable
-       |    +-----+           +-----+
-    20 |    |cn_a1|           |cn_b1|      (STAs/ CNs)
-       |    +-----+           +-----+
-       |
-       |       .                 .
-    10 |       .                 .         ... 5GHz WiFi
-       |       .                 .
-       |       .                 .
-       |    +------+          +------+
-     0 |    | ap_a |          | ap_b |     (APs/ DNs)
-       |    +------+          +------+
-       ----------------------------->
-     -15       0       25       50
+             h1a1              h1b1         h1c1 h2c1 h3c1    (CPEs)
+               |                 |            \    |    /
+       ^       |                 |             \   |   /     -- cable
+       |    +-----+           +-----+           +-----+
+    20 |    |cn_a1|           |cn_b1|           |cn_b2|     (STAs/ CNs)
+       |    +-----+           +-----+           +-----+
+       |       .                 .                 .
+       |       .                 .                 .
+    10 |       . CH:42           .      CH:58      .        ... 5GHz WiFi
+       |       .                 .                 .
+       |       .                 .                 .
+       |    +------+          +------+          +------+
+     0 |    | ap_a |          | ap_b |          | ap_c |    (APs/ DNs)
+       |    +------+          +------+          +------+
+       ----------------------------------------------->
+     -15       0       25       50       75        100
     '''
     def build(self, *args, **kwargs):
         # Segment A
@@ -64,13 +64,15 @@ class HybridBackupTerragraphTopo(Terratopo):
             Channel(36), Channel(40), Channel(44), Channel(48) # 20MHz
         ]
         dn_a = self.add_distribution_node_60('dn_a')
-        ap_a = self.add_smart_access_point('ap_a',
-                                           ssid='A',
-                                           available_channels=channel_list_a,
-                                           coordinates={'x': 0, 'y': 0})
+        ap_a = self.add_smart_access_point(
+            'ap_a',
+            ssid='A',
+            available_channels=channel_list_a,
+            coordinates={'x': 0, 'y': 0}
+        )
         self.add_ip_link(dn_a, ap_a)
-        cn_a1 = self.add_client_node('cn_a1',
-                                     coordinates={'x': 0, 'y': 20})
+
+        cn_a1 = self.add_client_node('cn_a1', coordinates={'x': 0, 'y': 20})
         self.add_terragraph_link(dn_a, cn_a1)
         self.add_wifi_link(ap_a, cn_a1)
 
@@ -82,19 +84,42 @@ class HybridBackupTerragraphTopo(Terratopo):
             Channel(52), Channel(56), Channel(60), Channel(64) # 20MHz
         ]
         dn_b = self.add_distribution_node_60('dn_b')
-        ap_b = self.add_smart_access_point('ap_b',
-                                           ssid='B',
-                                           available_channels=channel_list_b,
-                                           coordinates={'x': 50, 'y': 0})
+        ap_b = self.add_smart_access_point(
+            'ap_b',
+            ssid='B',
+            available_channels=channel_list_b,
+            coordinates={'x': 50, 'y': 0}
+        )
         self.add_ip_link(dn_b, ap_b)
-        cn_b1 = self.add_client_node('cn_b1',
-                                     coordinates={'x': 50, 'y': 20})
+
+        cn_b1 = self.add_client_node('cn_b1', coordinates={'x': 50, 'y': 20})
         self.add_terragraph_link(dn_b, cn_b1)
         self.add_wifi_link(ap_b, cn_b1)
 
 
+        # Segment C
+        channel_list_c = [
+            Channel(58), # 80MHz
+            Channel(54), Channel(62), # 40MHz
+            Channel(52), Channel(56), Channel(60), Channel(64) # 20MHz
+        ]
+        dn_c = self.add_distribution_node_60('dn_c')
+        ap_c = self.add_smart_access_point(
+            'ap_c',
+            ssid='C',
+            available_channels=channel_list_c,
+            coordinates={'x': 100, 'y': 0}
+        )
+        self.add_ip_link(dn_c, ap_c)
+
+        cn_c1 = self.add_client_node('cn_c1', coordinates={'x': 100, 'y': 20})
+        self.add_terragraph_link(dn_c, cn_c1)
+        self.add_wifi_link(ap_c, cn_c1)
+
+
         # Add WiFi 60GHz Links between DN_A and DN_B
         self.add_terragraph_link(dn_a, dn_b)
+        self.add_terragraph_link(dn_b, dn_c)
 
         # Add GW switch behind DN_A
         gw = self.add_gateway('s1')
@@ -102,24 +127,36 @@ class HybridBackupTerragraphTopo(Terratopo):
 
         # cn_a1 customer flows
         h1a1, s1a1 = self.add_customer_flow(cn_a1, gw,
-                                            suffix='1a1',
-                                            network=('10.145.128.0/24',
-                                                     'fd00:0:0:8101:8001::0/80'),
-                                            autostart_client=True)
-        h2a1, s2a1 = self.add_customer_flow(cn_a1, gw,
-                                            suffix='2a1',
-                                            network=('10.145.129.0/24',
-                                                     'fd00:0:0:8101:8002::0/80'),
-                                            autostart_client=False)
+            suffix='1a1',
+            network=('10.145.129.0/24',
+                     'fd00:0:0:8101:8001::0/80'),
+            autostart_client=True
+        )
 
         # cn_b1 customer flows
         h1b1, s1b1 = self.add_customer_flow(cn_b1, gw,
-                                            suffix='1b1',
-                                            network=('10.161.128.0/24',
-                                                     'fd00:0:0:8201:8001::0/80'),
-                                            autostart_client=True)
-        h2b1, s2b1 = self.add_customer_flow(cn_b1, gw,
-                                            suffix='2b1',
-                                            network=('10.161.129.0/24',
-                                                     'fd00:0:0:8201:8002::0/80'),
-                                            autostart_client=False)
+            suffix='1b1',
+            network=('10.161.129.0/24',
+                     'fd00:0:0:8201:8001::0/80'),
+            autostart_client=True
+        )
+
+        # cn_c1 customer flows
+        h1c1, s1c1 = self.add_customer_flow(cn_c1, gw,
+            suffix='1c1',
+            network=('10.177.129.0/24',
+                     'fd00:0:0:8301:8001::0/80'),
+            autostart_client=True
+        )
+        h2c1, s2c1 = self.add_customer_flow(cn_c1, gw,
+            suffix='2c1',
+            network=('10.177.130.0/24',
+                     'fd00:0:0:8301:8002::0/80'),
+            autostart_client=True
+        )
+        h3c1, s3c1 = self.add_customer_flow(cn_c1, gw,
+            suffix='3c1',
+            network=('10.177.131.0/24',
+                     'fd00:0:0:8301:8003::0/80'),
+            autostart_client=True
+        )
